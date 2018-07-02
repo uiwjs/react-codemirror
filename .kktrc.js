ba8ba688b559e9ff77f5421152b2080ea45d8a57
jaywcjlove <398188662@qq.com>
2018-07-02 12:00:52 +0800

    Update kkt config.

--- .kktrc.js
+++ .kktrc.js
@@ -41,37 +41,11 @@ module.exports = function (webpackConf, ServerConf) {
       }
       return item;
     });
-    if (webpackConf.mode === 'development') {
-      // console.log('webpackConf:', webpackConf);
-      // 开发模式下更改的 webpack 配置
-      // webpackConf.entry = './path/to/my/entry/file.js';
-      // webpackConf.output = {
-      //   library: "someLibName",
-      //   libraryTarget: "umd",
-      //   filename: "someLibName.js",
-      //   auxiliaryComment: "Test Comment"
-      // }
-      // webpackConf.output.auxiliaryComment =
-      // delete webpackConf.output;
-    }
+    if (webpackConf.mode === 'development') { }
     if (webpackConf.mode === 'production') {
       // 生产模式下更改的 webpack 配置
-      // webpackConf.entry = './path/to/my/entry/file.js';
-      // console.log('webpackConf:', webpackConf);
-      // webpackConf.output.path = 'wr';
-      // console.log('---->', __dirname)
-      // webpackConf.output.path = `${__dirname}${path.sep}wr5`;
-      // webpackConf.output.path = path.join(__dirname, 'wr2');
+      webpackConf.output.publicPath = '';
     }
     return webpackConf;
-  }
-  if (ServerConf) {
-    // ServerConf.proxy = {
-    //   '/api': {
-    //     target: 'http://127.0.0.1:1130',
-    //     changeOrigin: true,
-    //   },
-    // }
-    return ServerConf;
   }
 };
