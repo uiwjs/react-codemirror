3e260ca2c66b284b5137e51c67bea3fe7d56f9fb
jaywcjlove <398188662@qq.com>
2018-07-02 15:28:00 +0800

    Fix PropTypes did not introduce issue.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import 'codemirror/lib/codemirror.css';
 import CodeMirror from './CodeMirror';
 import loadMode from './loadMode';
