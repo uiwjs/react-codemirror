ba01097270c1f382677ee47b84b0f05eebdc93d6
jaywcjlove <398188662@qq.com>
2022-02-15 00:40:37 +0800

    feat: Add bundled version. #267

--- .kktrc.ts
+++ .kktrc.ts
@@ -3,50 +3,76 @@ import webpack, { Configuration } from 'webpack';
 import { LoaderConfOptions } from 'kkt';
 import lessModules from '@kkt/less-modules';
 import rawModules from '@kkt/raw-modules';
+import reactLibrary from '@kkt/react-library';
 import scopePluginOptions from '@kkt/scope-plugin-options';
 import pkg from './package.json';
 
 export default (conf: Configuration, env: 'development' | 'production', options: LoaderConfOptions) => {
-  conf = rawModules(conf, env, {
-    ...options,
-    test: /\.(txt|md)$/i,
-    esModule: true,
-  });
-  conf = scopePluginOptions(conf, env, {
-    ...options,
-    allowedFiles: [path.resolve(process.cwd(), 'src'), path.resolve(process.cwd(), 'README.md')],
-  });
-  conf = lessModules(conf, env, options);
-  // Get the project version.
-  conf.plugins!.push(
-    new webpack.DefinePlugin({
-      VERSION: JSON.stringify(pkg.version),
-    }),
-  );
-  if (env === 'production') {
-    conf.optimization = {
-      ...conf.optimization,
-      splitChunks: {
-        cacheGroups: {
-          reactvendor: {
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            name: 'react-vendor',
-            chunks: 'all',
-          },
-          refractor: {
-            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
-            name: 'refractor-vendor',
-            chunks: 'all',
-          },
-          codemirror: {
-            test: /[\\/]node_modules[\\/](@codemirror)[\\/]/,
-            name: 'codemirror-vendor',
-            chunks: 'all',
-          },
+  if (options.bundle) {
+    conf = lessModules(conf, env, options);
+    conf = reactLibrary(conf, env, {
+      ...options,
+      ...pkg,
+      name: 'codemirror',
+      main: 'dist/codemirror.js',
+      // webpack externals options
+      dependencies: {
+        react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react',
+        },
+        'react-dom': {
+          root: 'ReactDOM',
+          commonjs2: 'react-dom',
+          commonjs: 'react-dom',
+          amd: 'react-dom',
         },
       },
-    };
-    conf.output = { ...conf.output, publicPath: './' };
+    });
+  } else {
+    conf = rawModules(conf, env, {
+      ...options,
+      test: /\.(txt|md)$/i,
+      esModule: true,
+    });
+    conf = scopePluginOptions(conf, env, {
+      ...options,
+      allowedFiles: [path.resolve(process.cwd(), 'src'), path.resolve(process.cwd(), 'README.md')],
+    });
+    conf = lessModules(conf, env, options);
+    // Get the project version.
+    conf.plugins!.push(
+      new webpack.DefinePlugin({
+        VERSION: JSON.stringify(pkg.version),
+      }),
+    );
+    if (env === 'production') {
+      conf.optimization = {
+        ...conf.optimization,
+        splitChunks: {
+          cacheGroups: {
+            reactvendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+              name: 'react-vendor',
+              chunks: 'all',
+            },
+            refractor: {
+              test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+              name: 'refractor-vendor',
+              chunks: 'all',
+            },
+            codemirror: {
+              test: /[\\/]node_modules[\\/](@codemirror)[\\/]/,
+              name: 'codemirror-vendor',
+              chunks: 'all',
+            },
+          },
+        },
+      };
+      conf.output = { ...conf.output, publicPath: './' };
+    }
   }
   return conf;
 };
