7484096ec0ba74e69c029653cf81e9ca7a6db376
jaywcjlove <398188662@qq.com>
2022-02-24 15:46:38 +0800

    chore: Update dependencies & fix bundle import issue (#267).

--- .kktrc.ts
+++ .kktrc.ts
@@ -3,81 +3,74 @@ import webpack, { Configuration } from 'webpack';
 import { LoaderConfOptions } from 'kkt';
 import lessModules from '@kkt/less-modules';
 import rawModules from '@kkt/raw-modules';
-import reactLibrary from '@kkt/react-library';
 import scopePluginOptions from '@kkt/scope-plugin-options';
 import pkg from './package.json';
 
 export default (conf: Configuration, env: 'development' | 'production', options: LoaderConfOptions) => {
+  conf = lessModules(conf, env, options);
   if (options.bundle) {
-    conf = lessModules(conf, env, options);
-    conf = reactLibrary(conf, env, {
-      ...options,
-      ...pkg,
-      name: 'codemirror',
-      main: 'dist/codemirror.js',
-      // webpack externals options
-      dependencies: {
-        '@codemirror/basic-setup': {
-          root: ['CM', '@codemirror/basic-setup'],
-          commonjs: '@codemirror/basic-setup',
-          commonjs2: '@codemirror/basic-setup',
-        },
-        '@codemirror/state': {
-          root: ['CM', '@codemirror/state'],
-          commonjs: '@codemirror/state',
-          commonjs2: '@codemirror/state',
-        },
-        '@codemirror/view': {
-          root: ['CM', '@codemirror/view'],
-          commonjs: '@codemirror/view',
-          commonjs2: '@codemirror/view',
-        },
-        '@codemirror/theme-one-dark': {
-          root: ['CM', '@codemirror/theme-one-dark'],
-          commonjs: '@codemirror/theme-one-dark',
-          commonjs2: '@codemirror/theme-one-dark',
-        },
-        oneDark: {
-          root: ['CM', '@codemirror/theme-one-dark', 'oneDark'],
-        },
-        basicSetup: {
-          root: ['CM', '@codemirror/basic-setup', 'basicSetup'],
-        },
-        indentWithTab: {
-          root: ['CM', '@codemirror/commands', 'indentWithTab'],
-        },
-        keymap: {
-          root: ['CM', '@codemirror/view', 'keymap'],
-        },
-        placeholder: {
-          root: ['CM', '@codemirror/view', 'placeholder'],
-        },
-        ViewUpdate: {
-          root: ['CM', '@codemirror/view', 'ViewUpdate'],
-        },
-        EditorView: {
-          root: ['CM', '@codemirror/view', 'EditorView'],
-        },
-        StateEffect: {
-          root: ['CM', '@codemirror/state', 'StateEffect'],
-        },
-        EditorState: {
-          root: ['CM', '@codemirror/basic-setup', 'EditorState'],
-        },
-        react: {
-          root: 'React',
-          commonjs2: 'react',
-          commonjs: 'react',
-          amd: 'react',
-        },
-        'react-dom': {
-          root: 'ReactDOM',
-          commonjs2: 'react-dom',
-          commonjs: 'react-dom',
-          amd: 'react-dom',
-        },
+    conf.output!.library = '@uiw/codemirror';
+    conf.externals = {
+      '@codemirror/basic-setup': {
+        root: ['CM', '@codemirror/basic-setup'],
+        commonjs: '@codemirror/basic-setup',
+        commonjs2: '@codemirror/basic-setup',
       },
-    });
+      '@codemirror/state': {
+        root: ['CM', '@codemirror/state'],
+        commonjs: '@codemirror/state',
+        commonjs2: '@codemirror/state',
+      },
+      '@codemirror/view': {
+        root: ['CM', '@codemirror/view'],
+        commonjs: '@codemirror/view',
+        commonjs2: '@codemirror/view',
+      },
+      '@codemirror/theme-one-dark': {
+        root: ['CM', '@codemirror/theme-one-dark'],
+        commonjs: '@codemirror/theme-one-dark',
+        commonjs2: '@codemirror/theme-one-dark',
+      },
+      oneDark: {
+        root: ['CM', '@codemirror/theme-one-dark', 'oneDark'],
+      },
+      basicSetup: {
+        root: ['CM', '@codemirror/basic-setup', 'basicSetup'],
+      },
+      indentWithTab: {
+        root: ['CM', '@codemirror/commands', 'indentWithTab'],
+      },
+      keymap: {
+        root: ['CM', '@codemirror/view', 'keymap'],
+      },
+      placeholder: {
+        root: ['CM', '@codemirror/view', 'placeholder'],
+      },
+      ViewUpdate: {
+        root: ['CM', '@codemirror/view', 'ViewUpdate'],
+      },
+      EditorView: {
+        root: ['CM', '@codemirror/view', 'EditorView'],
+      },
+      StateEffect: {
+        root: ['CM', '@codemirror/state', 'StateEffect'],
+      },
+      EditorState: {
+        root: ['CM', '@codemirror/basic-setup', 'EditorState'],
+      },
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    };
   } else {
     conf = rawModules(conf, env, {
       ...options,
@@ -88,7 +81,6 @@ export default (conf: Configuration, env: 'development' | 'production', options:
       ...options,
       allowedFiles: [path.resolve(process.cwd(), 'src'), path.resolve(process.cwd(), 'README.md')],
     });
-    conf = lessModules(conf, env, options);
     // Get the project version.
     conf.plugins!.push(
       new webpack.DefinePlugin({
