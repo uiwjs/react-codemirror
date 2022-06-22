7c4e3664a9d2128c122dbe66e87e91b7b806d9b6
jaywcjlove <398188662@qq.com>
2022-06-22 11:41:03 +0800

    style(theme): update sublime theme style.

--- themes/sublime/src/index.ts
+++ themes/sublime/src/index.ts
@@ -14,14 +14,15 @@ export const sublime = createTheme({
     lineHighlight: '#00000059',
   },
   styles: [
-    { tag: t.comment, color: '#A2A9B5' },
+    { tag: [t.meta, t.comment], color: '#A2A9B5' },
     { tag: [t.attributeName, t.keyword], color: '#B78FBA' },
     { tag: t.function(t.variableName), color: '#5AB0B0' },
-    { tag: t.typeOperator, color: 'red' },
     { tag: [t.string, t.regexp, t.attributeValue], color: '#99C592' },
     { tag: t.operator, color: '#f47954' },
+    { tag: t.moduleKeyword, color: 'red' },
     { tag: [t.propertyName, t.typeName], color: '#629ccd' },
-    { tag: [t.tagName], color: '#E35F63' },
+    { tag: [t.tagName, t.modifier], color: '#E35F63' },
     { tag: [t.number, t.definition(t.tagName), t.className, t.definition(t.variableName)], color: '#fbac52' },
+    { tag: [t.atom, t.bool, t.special(t.variableName)], color: '#E35F63' },
   ],
 });
