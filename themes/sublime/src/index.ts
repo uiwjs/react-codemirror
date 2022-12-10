cbb41d1d66055d5b475b7df341b007f51f6e3dfb
jaywcjlove <398188662@qq.com>
2022-12-10 09:57:37 +0800

    fix(sublime): modify tagName color in sublime theme. (#430)

--- themes/sublime/src/index.ts
+++ themes/sublime/src/index.ts
@@ -23,10 +23,12 @@ export function sublimeInit(options?: CreateThemeOptions) {
       { tag: [t.string, t.regexp, t.attributeValue], color: '#99C592' },
       { tag: t.operator, color: '#f47954' },
       // { tag: t.moduleKeyword, color: 'red' },
-      { tag: [t.propertyName, t.typeName], color: '#629ccd' },
       { tag: [t.tagName, t.modifier], color: '#E35F63' },
       { tag: [t.number, t.definition(t.tagName), t.className, t.definition(t.variableName)], color: '#fbac52' },
       { tag: [t.atom, t.bool, t.special(t.variableName)], color: '#E35F63' },
+      { tag: t.variableName, color: '#539ac4' },
+      { tag: [t.propertyName, t.typeName], color: '#629ccd' },
+      { tag: t.propertyName, color: '#36b7b5' },
       ...styles,
     ],
   });
