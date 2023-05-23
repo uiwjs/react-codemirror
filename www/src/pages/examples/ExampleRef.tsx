250cf0410c3e57598c1065131635c4316e74a32a
jaywcjlove <398188662@qq.com>
2023-05-23 15:12:03 +0800

    website: update example.

--- www/src/pages/examples/ExampleRef.tsx
+++ www/src/pages/examples/ExampleRef.tsx
@@ -1,23 +1,22 @@
 import CodeMirror, { ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { langs } from '@uiw/codemirror-extensions-langs';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 export function Component() {
-  const $edit = useRef<ReactCodeMirrorRef>(null);
-  useEffect(() => {
-    console.log('$edit:', $edit);
-    setTimeout(() => {
-      console.log('$edit:view:', $edit.current?.view);
-      console.log('$edit:state:', $edit.current?.state);
-    }, 300);
-    console.log('$edit:', $edit.current?.view);
-  }, [$edit]);
+  let $edit = useRef<ReactCodeMirrorRef | null>(null);
+  function refCallack(editor: ReactCodeMirrorRef) {
+    if (!$edit.current && editor?.editor && editor?.state && editor?.view) {
+      // first time we got ref, similar to useEffect
+      console.log(editor); // do something with editor
+      $edit.current = editor; // store it
+    }
+  }
   return (
     <div>
       <CodeMirror
         value={`console.log('hello')`}
         theme="none"
-        ref={$edit}
+        ref={refCallack}
         height="400px"
         width="100%"
         style={{ margin: '0 0 23px 0', flex: 1 }}
