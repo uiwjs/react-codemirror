12bb2cefb4438151965b917a0af2e9987c4dbbdb
jaywcjlove <398188662@qq.com>
2023-09-20 10:35:22 +0800

    chore: add nextjs example. (#568)

---
+++ example/nextjs/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Editor from './Editor';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-between p-24">
+      <Editor />
+    </main>
+  );
+}
