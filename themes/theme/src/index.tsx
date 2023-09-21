7c5585ab0c8e08399877c9f59f3c7ab56054fad0
Ethan Van Der Heijden <84098504+Ethan-Vanderheijden@users.noreply.github.com>
2023-09-20 17:55:27 -0700

    fix: Fix double highlighting selection when using drawSelection. (#573)

--- themes/theme/src/index.tsx
+++ themes/theme/src/index.tsx
@@ -95,7 +95,7 @@ export const createTheme = ({ theme, settings = {}, styles = [] }: CreateThemeOp
 
   if (settings.selection) {
     themeOptions[
-      '&.cm-focused .cm-selectionBackground, &.cm-focused .cm-line::selection, & .cm-selectionLayer .cm-selectionBackground, .cm-content ::selection'
+      '&.cm-focused .cm-selectionBackground, & .cm-line::selection, & .cm-selectionLayer .cm-selectionBackground, .cm-content ::selection'
     ] = {
       background: settings.selection + ' !important',
     };
