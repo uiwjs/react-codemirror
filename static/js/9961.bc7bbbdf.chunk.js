fc8f5fa309c52aefb38c58a4758a26e21626e7bc
jaywcjlove <jaywcjlove@users.noreply.github.com>
2025-02-28 05:16:19 +0000

    v4.23.9 released v4.23.9 (#716) a5a2ab862dadd09a97d9ff3b595fffdf0216f970

---
+++ static/js/9961.bc7bbbdf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwww=self.webpackChunkwww||[]).push([[9961],{59961:(a,n,e)=>{e.r(n),e.d(n,{default:()=>f});const f="  SELECT ?x ?name\n  {\n    ?x  foaf:mbox <mailto:alice@example> .\n    ?x  foaf:knows ?a1 .\n    ?a1 foaf:knows ?a2 .\n    ?a2 foaf:name ?name .\n  }\n"}}]);
+//# sourceMappingURL=9961.bc7bbbdf.chunk.js.map
